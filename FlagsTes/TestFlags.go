7fb8dd76597c6cbe025a990a927c056e07a9413e
Panji Tri Wahyudi <richard.kennedy11316@gmail.com>
2021-12-22 12:09:41 +0700

    Test Flags with Alternative Argument
    Just testing to use `-n` instead `--n`

--- FlagsTes/TestFlags.go
+++ FlagsTes/TestFlags.go
@@ -1,3 +1,5 @@
+package main
+
 import (
 	"flag"
 	"fmt"
@@ -9,4 +11,19 @@ func main() {
 	name := flag.String("n", "", "Nama")
 	flag.Parse()
 	fmt.Printf("Hello, %s!\n", *name)
+
+	/*var nameArg string
+	for index, arg := range os.Args {
+		alt := "-n="
+		x := strings.Index(arg, alt)
+		if x > -1 {
+			nameArg = arg[x+len(alt):]
+			continue
+		}
+		if arg == "-n" || arg == "--n" {
+			nameArg = os.Args[index+1]
+			continue
+		}
+	}
+	fmt.Printf("Hello, %s!\n", nameArg) */
 }
